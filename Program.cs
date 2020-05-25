6ef9dc1fb22e3485b3482588e384b6b9bbd7d89d
görkem <berksahan@gmail.com>
2020-05-25 17:35:20 +0300

    done https://www.hackerrank.com/challenges/organizing-containers-of-balls/problem

--- Program.cs
+++ Program.cs
@@ -16,85 +16,81 @@ using System.Numerics;
 class Solution
 {
 
-    enum direction
+    static string organizingContainers(int[][] container)
     {
-        none,
-        up,
-        down,
-        right,
-        left
-    }
+        List<BigInteger> cont = new List<BigInteger>();
+        List<BigInteger> typeCont = new List<BigInteger>();
+        int cntContainer = container.Length;
 
-    static void move(ref int r, ref int c, direction d)
-    {
-        switch (d)
+        for (int i = 0; i < cntContainer; i++)
         {
-            case direction.up: r++; break;
-            case direction.down: r--; break;
-            case direction.right: c++; break;
-            case direction.left: c--; break;
-            case direction.none: break;
-        }
-    }
 
-    static void multiMove(List<direction> dAr, ref int x, ref int y)
-    {
-        foreach (var i in dAr)
-        {
-            move(ref x, ref y, i);
-        }
-    }
+            cont.Add(0);
+            typeCont.Add(0);
 
-    static int queensAttack(int n, int k, int r_q, int c_q, int[][] obstacles)
-    {
-        int cntMove = 0;
-        List<List<direction>> dList = new List<List<direction>>();
+            if (cntContainer != container[i].Length)
+                break;
 
-        dList.Add(new List<direction>() { direction.left });
-        dList.Add(new List<direction>() { direction.right });
-
-        dList.Add(new List<direction>() { direction.up });
-        dList.Add(new List<direction>() { direction.down });
-
-        dList.Add(new List<direction>() { direction.up, direction.left });
-        dList.Add(new List<direction>() { direction.up, direction.right });
-
-        dList.Add(new List<direction>() { direction.down, direction.left });
-        dList.Add(new List<direction>() { direction.down, direction.right });
-
-        HashSet<string> obs = new HashSet<string>();
-
-        if (k > 0)
-            foreach (var i in obstacles)
+            for (int j = 0; j < cntContainer; j++)
             {
-                obs.Add(i[0] + "_" + i[1]);
+                cont[i] += container[i][j];
+                typeCont[i] += container[j][i];
             }
+        }
 
-        foreach (var dr in dList)
+        if (cont.Count > 0)
+            cont.Sort();
+        if (typeCont.Count > 0)
+            typeCont.Sort();
+
+        if (isEqual(cont, typeCont))
+            return "Possible";
+        else
+            return "Impossible";
+
+    }
+
+    public static bool isEqual(List<BigInteger> ths, List<BigInteger> other)
+    {
+        bool ret = false;
+
+        if (ths.Count != other.Count)
+            ret = false;
+        else
         {
-            int r = r_q;
-            int c = c_q;
-            while (r <= n && c <= n && r > 0 && c > 0)
+            for (int i = 0; i < ths.Count; i++)
             {
-                multiMove(dr, ref r, ref c);
-                if (r <= n && c <= n && r > 0 && c > 0)
+                if (ths[i] != other[i])
                 {
-                    if (k > 0 && obs.Contains(r + "_" + c))
-                        break;
-                    else
-                        cntMove++;
+                    ret = false;
+                    break;
+                }
+                else
+                {
+                    ret = true;
                 }
             }
         }
 
-        return cntMove;
+        return ret;
 
     }
 
-
     static void Main(string[] args)
     {
-        var a = queensAttack(4, 0, 4, 4, null);
+        int[][] c = new int[8][];
+
+        c[0] = new int[] { 850352964, 977596446, 949048147, 948864413, 583894071, 690057713, 997747480, 989967717 };
+        c[1] = new int[] { 989808562, 768213277, 465807106, 806209383, 997601076, 726063750, 886274860, 999745463 };
+        c[2] = new int[] { 999716184, 614327240, 646233100, 701848586, 996640233, 637114525, 979053522, 973751653 };
+        c[3] = new int[] { 729579413, 623758391, 789451199, 998061161, 915515002, 887943421, 667982910, 663066401 };
+        c[4] = new int[] { 625660022, 983861176, 747822232, 924237583, 765182731, 879961473, 915988053, 973592376 };
+        c[5] = new int[] { 978486425, 911982749, 995853966, 373364101, 706580937, 995621049, 869416897, 906988203 };
+        c[6] = new int[] { 996021364, 982486194, 880450667, 971761433, 779568692, 990258135, 985311146, 547004947 };
+        c[7] = new int[] { 963237644, 954080173, 997900896, 551011238, 803702301, 931274261, 685754083, 585606717 };
+
+
+        var a = organizingContainers(c);
 
     }
 
